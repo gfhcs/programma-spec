d6ab96890d018f4b59cb1cca4f2dd2ad6bd19ec5
Gereon Fox <s9gefoxx@stud.uni-saarland.de>
2015-04-14 14:53:41 +0200

    More stub classes.

---
+++ lang-gen/ProgramGenerator.py
@@ -0,0 +1,21 @@
+'''
+Created on 14.04.2015
+
+@author: gereon
+'''
+
+class ProgramGenerator(object):
+    '''
+    Generates programs that satisfy various constraints of a given programming language.
+    '''
+
+    constraints: none, lexically correct, syntactically correct, semantically valid, custom
+    
+    parameters: count, randomized?
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ lang-gen/ssemantics/SemanticValidator.py
@@ -0,0 +1,17 @@
+'''
+Created on 14.04.2015
+
+@author: gereon
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
