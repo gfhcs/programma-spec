0ee7facfb9857ffef6636aad0c3d4b1c47a6fb1c
Gereon Fox <ger.fox07@t-online.de>
2015-04-17 15:11:20 +0200

    Fixed FileConstruct target handling.

--- lang-gen/construction/FileConstruct.py
+++ lang-gen/construction/FileConstruct.py
@@ -14,7 +14,7 @@ class FileConstruct(Construct):
     Represents a part of a document that consists of a directory.
     '''
 
-    def __init__(self, fileName, targets):
+    def __init__(self, fileName, targets=[]):
         super(FileConstruct, self).__init__(targets)
         self._fileName = fileName
         self._file = None
@@ -33,7 +33,7 @@ class FileConstruct(Construct):
     
     def dump(self, target=None):
         
-        if not(target is None or target in self._targets):
+        if not(target is None or len(self._targets) == 0 or target in self._targets):
             return
         
         with open(self.getFullPath(), 'w') as self._file:   
